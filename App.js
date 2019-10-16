aa3e2bdac071bf2d9bd6b7603482ee09a1deffde
Peter Banjo <peterbanjo@gmail.com>
2019-10-16 09:29:12 +0100

    clean-up. ignore compiled bundle because it changes on every build

--- App.js
+++ App.js
@@ -6,43 +6,51 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
 import MapView from "react-native-maps";
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
   android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+    "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu"
 });
 
 type Props = {};
 export default class App extends Component<Props> {
-  mapRef = (ref) => (this.mapRef = ref);
+  state = {
+    mapBoundaries: ""
+  };
+  mapRef = null;
+  setMapRef = ref => (this.mapRef = ref);
 
   handleRegionChange = async () => {
-    const map = mapRef.current;
-    await map.getMapBoundaries();
-
-    console.log('************************')
-    alert(typeof map.getMapBoundaries);
+    try {
+      const result = await this.mapRef.getMapBoundaries();
+      this.setState("mapBoundaries", JSON.stringify(result));
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   render() {
     return (
-      <MapView
-      ref={this.mapRef}
-      initialRegion={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}
-      onRegionChangeComplete={this.handleRegionChange}
-      style={styles.container}
-    />
-    )
+      <>
+        <Text>Map boundaries: {this.state.mapBoundaries}</Text>
+        <MapView
+          ref={this.setMapRef}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421
+          }}
+          onRegionChangeComplete={this.handleRegionChange}
+          style={styles.container}
+        />
+      </>
+    );
     // return (
     //   <View style={styles.container}>
     //     <Text style={styles.welcome}>Welcome to React Native!</Text>
@@ -56,18 +64,18 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
 });
